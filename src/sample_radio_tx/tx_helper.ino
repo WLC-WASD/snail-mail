0ce72b3b99516e52a02e7ffec8b17a20978c2803
Teschel6@bibbucket <elijah.tesch@mail.wlc.edu>
2019-02-11 20:55:00 -0600

    Added hex write out function

--- src/sample_radio_tx/tx_helper.ino
+++ src/sample_radio_tx/tx_helper.ino
@@ -1,10 +1,27 @@
 
+//write the corresponding hex pattern to the pins
+void h_write(int hex)
+{
+  int pin = 0;
+  for(int i = 1; i <= 8; i *= 2){
+    if (i & hex) {
+      b_write(PINS[pin],LOW);
+    }
+    else{
+      b_write(PINS[pin],HIGH);
+    }
+    pin++;
+  }
+}
+
 // Writes binary data to provided pin
 void b_write(int pin, bool data)
 {
   log("Binary Write (PIN " + String(pin) + ")");
   digitalWrite(pin, data);
 }
+
+
 
 // Batch Write data
 void groupWrite(bool data)
